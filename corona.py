b307dd72d4016eb4a4241b0c352c8977831c85c8
sahiltgi <abhi28005@gmail.com>
2020-03-21 01:35:04 +0530

    used BS4 to extract the data fom URL

--- corona.py
+++ corona.py
@@ -1,3 +1,9 @@
 import requests as re
+from bs4 import BeautifulSoup
 
-URL =
+URL = 'https://infographics.channelnewsasia.com/covid-19/map.html'
+page = re.get(URL)
+# print(page)
+
+soup = BeautifulSoup(page.content,'html.parser')
+print(soup)
