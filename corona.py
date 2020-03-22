8141854f862ecc867588127e8d384bf85249ab78
sahiltgi <abhi28005@gmail.com>
2020-03-22 11:40:01 +0530

    Created datadisplay.html file to display data

--- corona.py
+++ corona.py
@@ -5,5 +5,7 @@ URL = 'https://infographics.channelnewsasia.com/covid-19/map.html'
 page = re.get(URL)
 # print(page)
 
-soup = BeautifulSoup(page.content,'html.parser')
-print(soup)
+soup = BeautifulSoup(page.content, 'html.parser')
+# print(soup)
+results = soup.find(id('dataContainer'))
+print(results.pr)
