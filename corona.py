95c331494057506f3e0b7f6e8a803eb0d20f98c9
sahiltgi <abhi28005@gmail.com>
2020-04-10 13:13:42 +0530

    Importing modules

--- corona.py
+++ corona.py
@@ -1,11 +1,3 @@
-import requests as re
-from bs4 import BeautifulSoup
+import requests
+from  bs4 import BeautifulSoup
 
-URL = 'https://infographics.channelnewsasia.com/covid-19/map.html'
-page = re.get(URL)
-# print(page)
-
-soup = BeautifulSoup(page.content, 'html.parser')
-# print(soup)
-results = soup.find(id('dataContainer'))
-print(results.pr)
