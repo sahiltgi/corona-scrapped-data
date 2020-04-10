ea419b1d621592790dc23f1a4523db580fd02ead
sahiltgi <abhi28005@gmail.com>
2020-04-10 13:18:28 +0530

    Beautify the code

--- corona.py
+++ corona.py
@@ -1,11 +1,13 @@
 import requests
-from  bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 
 url = "https://www.worldometers.info/coronavirus/"
 r = requests.get(url)
-s = BeautifulSoup(r.text,"html.parser")
-data = s.find_all("div",class_ = "maincounter-number")
+s = BeautifulSoup(r.text, "html.parser")
+data = s.find_all("div", class_="maincounter-number")
 
-print("Total Cases:- ",data[0].text.strip())
-print("Total Deaths:- ",data[1].text.strip())
-print("Total Recovered:- ",data[2].text.strip())
+print("===========================================OUTPUT=================================================")
+
+print("Total Cases:- ", data[0].text.strip())
+print("Total Deaths:- ", data[1].text.strip())
+print("Total Recovered:- ", data[2].text.strip())
